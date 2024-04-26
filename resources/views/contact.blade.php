2a4a5654ab9c31a147c4e51191873a430cb78fb4
hafiz-aa <mhafiz.aa@gmail.com>
2024-04-26 08:38:37 +0700

    Update contact css

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <!-- Tailwind CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <!-- Font Awesome Icons CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+
+
     <title>Contact</title>
 </head>
 <body>
@@ -15,12 +21,18 @@
 
     <!-- Contact -->
     <div class="contact">
-        <h1>Contact Me</h1>
+        <h1 class="text-3xl">Contact Me</h1>
         <div class="social-links">
-            <a href="https://www.linkedin.com/in/mhafiz-aa/" class="linkedin" target="_blank">LinkedIn</a>
-            <a href="https://www.facebook.com/mhafiz/" class="fb" target="_blank">Facebook</a>
-            <a href="https://www.instagram.com/hafiz_aa" class="ig" target="_blank">Instagram</a>
+            <a href="https://www.linkedin.com/in/mhafiz-aa/" class="btn btn-lg mr-2" target="_blank"><i class="fab fa-linkedin fa-lg"></i></a>
+            <a href="https://www.facebook.com/mhafiz/" class="btn btn-lg mr-2" target="_blank"><i class="fab fa-facebook-square fa-lg"></i></a>
+            <a href="https://www.instagram.com/hafiz_aa" class="btn btn-lg mr-2" target="_blank"><i class="fab fa-instagram fa-lg"></i></a>
         </div>
+
     </div>
+
+    <!-- Font Awesome Icons JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js"></script>
+
+
 </body>
 </html>
