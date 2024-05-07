1529fc48b6abfcfdd0777bcff3626dacb227fbc0
hafiz-aa <mhafiz.aa@gmail.com>
2024-05-07 10:34:55 +0700

    Update view data

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,18 +2,15 @@
     
   <x-slot:title>{{ $title }}</x-slot:title>
    
-  @foreach ($posts as $post)
-   
     <article class="p-6 mb-6 bg-white rounded-lg shadow-md">
-      <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
+      
         <h2 class="mb-1 text-lg tracking-tight font-bold text-gray-900">{{ $post['title']}} </h2>
-      </a>
+     
       <div class="text-sm text-gray-600">
         <a href="#"></a>{{ $post['author']}} | 1 Januari 2024
       </div>
-      <p class="my-3 font-light">{{ Str::limit($post['body'], 40, '...')}}</p>
-      <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+      <p class="my-3 font-light">{{$post['body']}}</p>
+      <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo;Back to posts</a>
     </article>
-    @endforeach
   
   </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
+use function PHPUnit\Framework\returnSelf;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -13,18 +16,47 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog Page', 'posts' => [
         [
+            'id' => 1,
+            'slug' => 'article-1',
             'title' => 'Article 1',
             'author' => 'Muhammad Hafiz',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed veritatis, beatae officia magnam et quasi provident inventore asperiores ea quas.',
         ],
         [
-        'title' => 'Article 2',
-        'author' => 'Muhammad Hafiz',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, voluptate. Unde cupiditate rerum deleniti, ducimus voluptates ipsam error. Aperiam dolorum amet voluptatibus neque vero!'
+            'id' => 2,
+            'slug' => 'article-2',
+            'title' => 'Article 2',
+            'author' => 'Muhammad Hafiz',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, voluptate. Unde cupiditate rerum deleniti, ducimus voluptates ipsam error. Aperiam dolorum amet voluptatibus neque vero!'
         ],
         ]
     ]);
 });
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'article-1',
+            'title' => 'Article 1',
+            'author' => 'Muhammad Hafiz',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed veritatis, beatae officia magnam et quasi provident inventore asperiores ea quas.',
+        ],
+        [
+            'id' => 2,
+            'slug' => 'article-2',
+            'title' => 'Article 2',
+            'author' => 'Muhammad Hafiz',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, voluptate. Unde cupiditate rerum deleniti, ducimus voluptates ipsam error. Aperiam dolorum amet voluptatibus neque vero!'
+        ],
+    ];
+
+    $post = Arr::first($posts, function ($post) use($slug) { 
+        return $post['slug'] == $slug;
+    });
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact Page']);
 });
