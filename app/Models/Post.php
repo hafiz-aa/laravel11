f448673dd94ebe088670149ebe654d7f6dd4e614
hafiz-aa <mhafiz.aa@gmail.com>
2024-05-17 10:01:30 +0700

    Update Eloquent ORM

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,35 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Post
+class Post extends Model
 {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'article-1',
-                'title' => 'Article 1',
-                'author' => 'Muhammad Hafiz',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed veritatis, beatae officia magnam et quasi provident inventore asperiores ea quas.',
-            ],
-            [
-                'id' => 2,
-                'slug' => 'article-2',
-                'title' => 'Article 2',
-                'author' => 'Muhammad Hafiz',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, voluptate. Unde cupiditate rerum deleniti, ducimus voluptates ipsam error. Aperiam dolorum amet voluptatibus neque vero!'
-            ],
-        ];
-    }
-
-    public static function find($slug) {
-        $post = Arr::first(static::all(), fn ($post) => $post['slug'] === $slug);
-
-        if(!$post) {
-            abort(404);
-     }
-    return $post;
-  }   
+    
 }
