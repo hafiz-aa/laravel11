7b264d8cda1ff732d30b6ba42b5e3d66eeb85145
hafiz-aa <mhafiz.aa@gmail.com>
2024-05-25 07:46:43 +0700

    Update model post and user., add relation. Update view add authors.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,8 +8,8 @@
       <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
         <h2 class="mb-1 text-lg tracking-tight font-bold text-gray-900">{{ $post['title']}} </h2>
       </a>
-      <div class="text-sm text-gray-600">
-        <a href="#"></a>{{ $post['author']}} | {{ $post['created_at']-> format('d M Y') }}
+      <div class="text-sm text-gray-600 ">
+        <a href="/authors/{{ $post->author->id}}" class="hover:underline">{{ $post->author->name}} </a>| {{ $post['created_at']-> format('d M Y') }}
       </div>
       <p class="my-3 font-light">{{ Str::limit($post['body'], 40, '...')}}</p>
       <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
