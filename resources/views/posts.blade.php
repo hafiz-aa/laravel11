24617907f0c6ff09d1a1f2c700ee2a7a6085da47
hafiz-aa <mhafiz.aa@gmail.com>
2024-05-28 10:38:59 +0700

    Add username

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,11 +5,12 @@
   @foreach ($posts as $post)
    
     <article class="p-6 mb-6 bg-white rounded-lg shadow-md">
-      <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
+      <a href="/posts/{{ $post['slug'] }}" class="hover:underline"></a>
         <h2 class="mb-1 text-lg tracking-tight font-bold text-gray-900">{{ $post['title']}} </h2>
-      </a>
-      <div class="text-sm text-gray-600 ">
-        <a href="/authors/{{ $post->author->id}}" class="hover:underline">{{ $post->author->name}} </a>| {{ $post['created_at']-> format('d M Y') }}
+      
+      <div>
+        By <a href="/authors/{{ $post->author->username}}" class="hover:underline text-sm text-gray-600">{{ $post->author->name}} </a> in 
+        <a href="#" class="hover:underline text-sm text-gray-600"> Web Programming</a> | {{ $post['created_at']-> format('d M Y') }}
       </div>
       <p class="my-3 font-light">{{ Str::limit($post['body'], 40, '...')}}</p>
       <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
