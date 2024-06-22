39e64355b6a78f457ee1808ce6ed858379795618
hafiz-aa <mhafiz.aa@gmail.com>
2024-06-23 06:53:26 +0700

    Update post layout

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,20 +7,6 @@
        
       @foreach ($posts as $post)
    
-      {{-- <article class="p-6 mb-6 bg-white rounded-lg shadow-md">
-        <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
-          <h2 class="mb-1 text-lg tracking-tight font-bold text-gray-900">{{ $post['title']}} </h2></a>
-          
-        
-        <div>
-          By <a href="/authors/{{ $post->author->username}}" class="hover:underline text-sm text-gray-600">{{ $post->author->name}} </a> in 
-          <a href="/categories/{{ $post->category->slug}}" class="hover:underline text-sm text-gray-600"> {{ $post->category->name}}</a> | {{ $post['created_at']-> format('d M Y') }}
-        </div>
-        <p class="my-3 font-light">{{ Str::limit($post['body'], 40, '...')}}</p>
-        <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
-      </article> --}}
-
-   
         <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
             <div class="flex justify-between items-center mb-5 text-gray-500">
               <a href="/categories/{{ $post->category->slug}}">
