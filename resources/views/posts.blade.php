cdc143ee59557424ca591016eb0a7f04cb43bbdf
hafiz-aa <mhafiz.aa@gmail.com>
2024-06-27 11:48:55 +0700

    Add search query for post, category and author

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,6 +6,12 @@
   <div class="py-4 px-4 mx-auto max-w-screen-xl lg:px-6">
     <div class="mx-auto max-w-screen-md sm:text-center">
         <form>
+          @if (request('category'))
+          <input type="hidden" name="category" value="{{ request('category') }}">
+          @endif
+          @if (request('author'))
+          <input type="hidden" name="author" value="{{ request('author') }}">
+          @endif
             <div class="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
                 <div class="relative w-full">
                     <label for="search" class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Search</label>
@@ -28,11 +34,11 @@
   <div class="py-8 px-4 smx-auto max-w-screen-xl lg:py-8 lg:px-0">
     <div class="grid gap-8 lg:grid-cols-3">
        
-      @foreach ($posts as $post)
+      @forelse ($posts as $post)
    
         <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
             <div class="flex justify-between items-center mb-5 text-gray-500">
-              <a href="/categories/{{ $post->category->slug}}">
+              <a href="/posts?category={{ $post->category->slug}}">
                 <span class="bg-{{ $post->category->color }}-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
                   {{ $post->category->name}}
                 </span>
@@ -42,7 +48,7 @@
             <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="/posts/{{ $post->slug }}" class="hover:underline">{{ $post->title }}</a></h2>
             <p class="mb-5 font-light text-gray-500 dark:text-gray-400">{{ Str::limit($post->body, 120, '...')}}</p>
             <div class="flex justify-between items-center">
-              <a href="/authors/{{ $post->author->username}}" >
+              <a href="/posts?author={{ $post->author->username}}" >
                 <div class="flex items-center space-x-3">
                     <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="{{ $post->author->name}}" />
                     <span class="font-medium text-sm dark:text-white">
@@ -56,8 +62,13 @@
               </a>
             </div>
         </article> 
-                            
-      @endforeach
+    
+      @empty
+      <div class="p-6 ">
+        <p class="font-semibold text-gray-500 dark:text-gray-400">Article not found</p>
+        <a href="/posts" class="block text-blue-500 hover:underline">&laquo; back to all posts</a>
+      </div>
+      @endforelse
    </div>  
   </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    $posts = Post::latest();
-
-    if (request('search')){
-        $posts-> where('title', 'like', '%' . request('search') . '%');
-    }
-    //$posts = Post::with(['author', 'category'])->latest()->get();
-    
-    return view(
-        'posts', ['title' => 'Blog Page', 
-        'posts' => $posts->get(),
-    ]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->get()]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
